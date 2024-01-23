d0013ea898385f160acbc747ba6ce721bf51c686
shaam <msyamil.hamami@gmail.com>
2024-01-23 11:19:41 +0700

    Add script to generate go module

---
+++ generate-module.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create directory
+mkdir $1
+
+# Create module
+cd $1
+go mod init $1
+
+# Create main.go
+touch main.go
+
+# Add package main
+echo "package main" >> main.go
+
+# Add to workspace
+cd ..
+go work use $1

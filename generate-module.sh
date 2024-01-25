ab2b59b3e1ef72bb345d4cd6d6fb3e0f43e74eef
shaam <msyamil.hamami@gmail.com>
2024-01-25 17:37:56 +0700

    Update script

--- generate-module.sh
+++ generate-module.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Create directory
 mkdir $1
-
-# Create module
 cd $1
 go mod init $1
-
-# Create main.go
 touch main.go
 
-# Add package main
 echo "package main" >> main.go
+echo "" >> main.go
+echo "func main() {" >> main.go
+echo "" >> main.go
+echo "}" >> main.go
 
-# Add to workspace
 cd ..
 go work use $1
 

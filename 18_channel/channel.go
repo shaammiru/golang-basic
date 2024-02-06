4179ae1b213fa78946bcc4870bf6e95c2da6b6d5
shaam <msyamil.hamami@gmail.com>
2024-02-06 13:35:01 +0700

    Update channel code

--- 18_channel/channel.go
+++ 18_channel/channel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -38,9 +39,83 @@ func bufferedChannel() {
 		messages <- each
 	}
 
-	time.Sleep(1 * time.Nanosecond)
+	time.Sleep(1 * time.Millisecond)
 }
 
 func selectChannel() {
+	numbers := []int{3, 1, 9, 7, 2, 3, 5, 2}
+	fmt.Println("Numbers :", numbers)
 
+	ch1 := make(chan float64)
+	go func(numbers []int, ch chan float64) {
+		sum := 0
+		for _, e := range numbers {
+			sum += e
+		}
+		ch <- float64(sum) / float64(len(numbers))
+	}(numbers, ch1)
+
+	ch2 := make(chan int)
+	go func(numbers []int, ch chan int) {
+		max := numbers[0]
+		for _, e := range numbers {
+			if max < e {
+				max = e
+			}
+		}
+		ch <- max
+	}(numbers, ch2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case avgCh := <-ch1:
+			fmt.Printf("Avg : %.2f\n", avgCh)
+		case maxCh := <-ch2:
+			fmt.Printf("Max : %d\n", maxCh)
+		}
+	}
+}
+
+func rangeCloseChannel() {
+	messages := make(chan string)
+	// chan<- untuk mengirim data ke channel (write-only)
+	// jadi fungsi ini hanya bisa mengirim data ke channel
+	go func(ch chan<- string) {
+		for i := 0; i < 10; i++ {
+			ch <- fmt.Sprintf("Message %d", i)
+		}
+		close(ch)
+	}(messages)
+
+	// <-chan untuk menerima data dari channel (read-only)
+	// jadi fungsi ini hanya bisa menerima data dari channel
+	func(ch <-chan string) {
+		for msg := range ch {
+			fmt.Println(msg)
+		}
+	}(messages)
+}
+
+func channelTimeout() {
+	var messages = make(chan int)
+
+	go func(ch chan<- int) {
+		randomizer := rand.New(rand.NewSource(time.Now().Unix()))
+
+		for i := 0; true; i++ {
+			ch <- i
+			time.Sleep(time.Duration(randomizer.Int()%10+1) * time.Second)
+		}
+	}(messages)
+
+loop:
+	for {
+		select {
+		case data := <-messages:
+			fmt.Println("Receive data", data)
+		case <-time.After(time.Second * 5):
+			fmt.Println("Timeout! No activities for 5 seconds")
+			break loop
+		}
+	}
 }
--- 18_channel/main.go
+++ 18_channel/main.go
@@ -19,6 +19,25 @@ func main() {
 	unbufferedChannel()
 
 	// Buffered Channel Example
+	// Buffered channel adalah channel yang memiliki buffer
+	// Ketika membuat buffered channel, kita perlu menentukan kapasitas buffer
+	// Ketika buffer penuh, maka channel akan menjadi blocking
 	fmt.Println(">> Buffered Channel Example <<")
 	bufferedChannel()
+
+	// Select Channel Example
+	// Select adalah cara untuk melakukan komunikasi dengan channel secara non-blocking
+	fmt.Println(">> Select Channel Example <<")
+	selectChannel()
+
+	// Range Close Channel Example
+	// Range adalah cara untuk melakukan iterasi data dari channel
+	// Close adalah cara untuk memberitahukan bahwa channel sudah selesai digunakan
+	fmt.Println(">> Range Close Channel Example <<")
+	rangeCloseChannel()
+
+	// Channel Timeout Example
+	// Timeout adalah cara untuk menghentikan proses yang terlalu lama
+	fmt.Println(">> Channel Timeout Example <<")
+	channelTimeout()
 }
